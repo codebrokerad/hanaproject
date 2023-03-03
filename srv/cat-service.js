17ad4f2f642952b448f3234661a219dde900099f
codebrokerad <utuman@windowslive.com>
2023-03-03 15:14:51 +0000

    custom error handling for services

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -13,4 +13,25 @@ module.exports = cds.service.impl(function () {
             this.emit('prod_Change', header)
         })
     })
+
+    /**
+     * Custom error handler
+     *
+     * throw a new error with: throw new Error('something bad happened');
+     *
+     **/
+    this.on("error", (err, req) => {
+        switch (err.message) {
+          case "ENTITY_ALREADY_EXISTS":
+            err.message = `An error occurred. Please retry. Error code: ENTITY_ALREADY_EXISTS. Error description: The entry already exists.`;
+            break;
+          default:
+            if (!err.message.includes("Technical explanation of error")) {
+              err.message = `An error occurred. Please retry. Technical explanation of error: ${err.message}`;
+            }
+            break;
+        }
+      });
+      
+      
 })
